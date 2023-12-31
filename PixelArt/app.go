8c81464554463509b2c488070edd90532c625925
Enzzx <101784007+Enzzx@users.noreply.github.com>
2023-12-31 00:38:46 -0300

    Update app.go

--- PixelArt/app.go
+++ PixelArt/app.go
@@ -3,11 +3,41 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"github.com/gorilla/websocket"
 )
 
+var upgrader = websocket.Upgrader{
+	CheckOrigin: func(r * http.Request) bool {
+		return true
+	},
+}
+
+var clients []websocket.Conn
+
+func handleSocket(w http.ResponseWriter, r *http.Request) {
+	socket, _ := upgrader.Upgrade(w, r, nil)
+	defer socket.Close()
+
+	clients = append(clients, *socket)
+
+	for {
+		messageType, p, err := socket.ReadMessage()
+		if err != nil {
+			fmt.Print(err)
+			return
+		}
+		for _, client := range clients {
+			err = client.WriteMessage(messageType, p)
+		}
+	}
+}
+
 func main() {
+	http.HandleFunc("/room", handleSocket)
+	
 	http.Handle("/", http.FileServer(http.Dir(".")))
 
+	fmt.Println("servidor tá rodando")
 	server := http.ListenAndServe(":8080", nil)
 	if server != nil {
 		fmt.Println("erro no servidor")
